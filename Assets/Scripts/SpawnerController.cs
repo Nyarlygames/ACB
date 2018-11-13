48e173e3a38db3f976507a36c4a961ced32f851c
Nyarlygames <nanacry@gmail.com>
2018-11-13 13:16:42 +0100

    final sizes & fix pos zero

--- Assets/Scripts/SpawnerController.cs
+++ Assets/Scripts/SpawnerController.cs
@@ -13,11 +13,15 @@ public class SpawnerController : MonoBehaviour {
     public void SetObjects(Transform cam, Transform ruck, Transform ball)
     {
         if (direction == 2)
+        {
             cam.position = gameObject.transform.position + new Vector3(5.0f, 15.0f, -25.0f);
+        }
         else
+        {
+            ball.position = ball.position + new Vector3(-2.0f, 0.0f, 0.0f);
             cam.position = gameObject.transform.position + new Vector3(-5.0f, 15.0f, -25.0f);
+        }
         ruck.position = gameObject.transform.position;
-       // ball.position = gameObject.transform.position + new Vector3(0.0f,-1.0f,0.0f);
     }
 
     void Update()
